b106a495d060ef6a67c69c16ed606fd9745d4756
viggy <dviglione02@gmail.com>
2020-03-20 10:30:40 -0400

    filtering out response to determine whether test passed or not

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import * as path from 'path';
 import { runCLI } from 'jest';
 
-runCLI({}, ['.']).then( resp => {
-    console.log('And the resp: ', resp);
-}).catch(e => console.log(e));
+( async () => {
+    try {
+        const resp = await runCLI({}, ['.']);
+        const result = resp.results.testResults.filter( (result) => path.basename(result.testFilePath) === 'example.test.js' )
+        const success = result.every( test => test.numFailingTests === 0)
+        console.log('the resp: ',  success);
+    } catch(e) {
+        console.log('ERROR: ', e);
+    }
+})()
